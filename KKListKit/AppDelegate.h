54fbb52941bccceaada5bd19790ec3cb8aef3a9f
lvhongyang <lvhongyang@kkworld.com>
2018-06-13 22:45:31 +0800

    Initial Commit

---
+++ KKListKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KKListKit
+//
+//  Created by LV on 2018/6/13.
+//  Copyright © 2018年 LV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KKListKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KKListKit
+//
+//  Created by LV on 2018/6/13.
+//  Copyright © 2018年 LV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
